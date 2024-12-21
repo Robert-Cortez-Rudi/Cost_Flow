a181a3e940e78ef2d19fb1405ef70c00feaa3bec
Robert Rudi <robertcortezrudi@gmail.com>
2024-12-20 22:05:02 -0300

    Update user model

--- usuario/models.py
+++ usuario/models.py
@@ -1,10 +1,21 @@
 from django.db import models
+from django.contrib.auth.hashers import make_password, check_password
 
 class User(models.Model):
     username = models.CharField(max_length=50)
-    email = models.EmailField()
-    password = models.CharField(max_length=30)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+    first_name = models.CharField(max_length=50, blank=True)
+    last_name = models.CharField(max_length=50, blank=True)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now=True)
+
+    def set_password(self, raw_password):
+        self.password = make_password(raw_password)
+
+    def check_password(self, raw_password):
+        return check_password(raw_password, self.password)
 
     def __str__(self):
-        return f"{self.username}"
-# APRIMORAR 
+        return f"{self.username} - {self.email}"
