2d25365ce749f4555348a9cf3976914b402a619d
Robert Rudi <robertcortezrudi@gmail.com>
2024-12-22 19:46:23 -0300

    Add URL patterns for user authentication and profile management

--- usuario/urls.py
+++ usuario/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
+from usuario.views import home, login, register, logout, profile, edit_profile
 
 urlpatterns = [
-    
+    path("", home, name="home"),
+    path("login/", login, name="login"),
+    path("register/", register, name="register"),
+    path("logout/", logout, name="logout"),
+    path("profile/", profile, name="profile"),
+    path("profile/edit/", edit_profile, name="edit_profile")
 ]
