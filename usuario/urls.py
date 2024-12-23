885e45d2e622fb88a3fe97379f3910e32d5de656
Robert Rudi <robertcortezrudi@gmail.com>
2024-12-23 18:30:05 -0300

    Rename login and logout views for clarity and add delete profile URL

--- usuario/urls.py
+++ usuario/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from usuario.views import home, login, register, logout, profile, edit_profile
+from usuario.views import home, login_user, register, logout_user, profile, edit_profile, delete_profile
 
 urlpatterns = [
     path("", home, name="home"),
-    path("login/", login, name="login"),
+    path("login/", login_user, name="login"),
     path("register/", register, name="register"),
-    path("logout/", logout, name="logout"),
+    path("logout/", logout_user, name="logout"),
     path("profile/", profile, name="profile"),
-    path("profile/edit/", edit_profile, name="edit_profile")
+    path("profile/edit/", edit_profile, name="edit_profile"),
+    path("profile/delete", delete_profile, name="delete_profile")
 ]
