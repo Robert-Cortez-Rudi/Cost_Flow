7c5ae95994215eac25b852d85c49d0f7f923bfae
Robert Rudi <robertcortezrudi@gmail.com>
2024-12-23 18:31:27 -0300

    Enhance user registration form by adding fields for first name, last name, and email; improve placeholder attributes for better user experience.

--- usuario/forms/register_form.py
+++ usuario/forms/register_form.py
@@ -2,6 +2,14 @@ from django import forms
 from ..models import User
 
 class UserSignForm(forms.ModelForm):
+    username = forms.CharField(max_length=30, widget=forms.TextInput(attrs={"placeholder": "Usuário", "class": "form-control"}),
+                               label="Usuário")
+    first_name = forms.CharField(max_length=30, widget=forms.TextInput(attrs={"placeholder": "Nome", "class": "form-control"}),
+                                label="Nome")
+    last_name = forms.CharField(max_length=30, widget=forms.TextInput(attrs={"placeholder": "Sobrenome", "class": "form-control"}),
+                                label="Sobrenome")
+    email = forms.EmailField(widget=forms.EmailInput(attrs={"placeholder": "Email", "class": "form-control"}), 
+                                label="Email")
     password = forms.CharField(min_length=8, widget=forms.PasswordInput(attrs={"placeholder": "Senha", "class": "form-control"}), 
                                label="Senha", strip=False)
     confirm_password = forms.CharField(min_length=8, widget=forms.PasswordInput(attrs={"placeholder": "Confirmar Senha", "class": "form-control"}), 
@@ -11,24 +19,9 @@ class UserSignForm(forms.ModelForm):
         model = User
         fields = ["username", "email", "first_name", "last_name", "password", "confirm_password"]
 
-    def __init__(self, *args, **kyargs):
-        super().__init__(*args, **kyargs)
-        self.fields["username"].widget.attrs["autofocus"] = True
-        self.fields["username"].widget.attrs["class"] = "form-control"
-        self.fields["username"].widget.attrs["placeholder"] = "Usuário"
-
-        self.fields["email"].widget.attrs["class"] = "form-control"
-        self.fields["email"].widget.attrs["placeholder"] = "Email"
-
-        self.fields["first_name"].widget.attrs["class"] = "form-control"
-        self.fields["first_name"].widget.attrs["placeholder"] = "Primeiro Nome"
-
-        self.fields["last_name"].widget.attrs["class"] = "form-control"
-        self.fields["last_name"].widget.attrs["placeholder"] = "Sobrenome"
-
 
     def clean_email(self):
-        email = self.cleaned_data().get("email")
+        email = self.cleaned_data.get("email")
         if User.objects.filter(email=email).exists():
             raise forms.ValidationError("Email já cadastrado!")
         return email
