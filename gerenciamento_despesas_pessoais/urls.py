f70c448987b6df3f65565e7ab4a6604413f03666
Robert Rudi <robertcortezrudi@gmail.com>
2024-12-24 18:35:34 -0300

    Add URL routing for despesas app

--- gerenciamento_despesas_pessoais/urls.py
+++ gerenciamento_despesas_pessoais/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("usuario.urls"))
+    path("", include("usuario.urls")),
+    path("despesas/", include("despesas.urls")),
 ]
