7bdbcb13d89f18c3e47afe68cfe969d2ebc4f71f
Robert Rudi <robertcortezrudi@gmail.com>
2024-12-28 00:16:23 -0300

    Update DespesaForm to remove usuario field and enhance widget styling

--- despesas/forms/despesa_form.py
+++ despesas/forms/despesa_form.py
@@ -4,10 +4,9 @@ from ..models import Despesa
 class DespesaForm(forms.ModelForm):
     class Meta:
         model = Despesa
-        fields = ['usuario', 'categoria', 'valor', 'descricao']
+        fields = ['categoria', 'valor', 'descricao']
         widgets = {
-            'usuario': forms.HiddenInput(),  # Esconde o campo de usuário no formulário
-            'categoria': forms.Select(choices=Despesa.CATEGORIAS),
-            'valor': forms.NumberInput(attrs={'step': '0.01'}),
-            'descricao': forms.TextInput(attrs={'maxlength': 255}),
+            'categoria': forms.Select(attrs={'class': 'form-control'}),
+            'valor': forms.NumberInput(attrs={'class': 'form-control', 'step': '0.01'}),
+            'descricao': forms.TextInput(attrs={'class': 'form-control', 'maxlength': 255}),
         }
