1e32d699f2d8c4ccdf8e23584df0b26f705bab96
Robert Rudi <robertcortezrudi@gmail.com>
2024-12-28 00:15:41 -0300

    Refactor URL names for despesas app to use underscores

--- despesas/urls.py
+++ despesas/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import despesa_create, despesa_list, despesa_update, despesa_delete
 
 urlpatterns = [
-    path('', despesa_list, name='despesa-list'),
-    path('create/', despesa_create, name='despesa-create'),
-    path('update/<int:pk>/', despesa_update, name='despesa-update'),
-    path('delete/<int:pk>/', despesa_delete, name='despesa-delete'),
+    path('', despesa_list, name='despesa_list'),
+    path('create/', despesa_create, name='despesa_create'),
+    path('update/<int:pk>/', despesa_update, name='despesa_update'),
+    path('delete/<int:pk>/', despesa_delete, name='despesa_delete'),
 ]
