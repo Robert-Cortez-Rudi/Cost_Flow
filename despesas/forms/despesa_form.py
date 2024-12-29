723eb92eaedf337c55d26d66771cfdc8f98591cb
Robert Rudi <robertcortezrudi@gmail.com>
2024-12-29 15:05:34 -0300

    Add EntradaForm to despesa_form for income tracking

--- despesas/forms/despesa_form.py
+++ despesas/forms/despesa_form.py
@@ -1,6 +1,17 @@
 from django import forms
 from ..models import Despesa
 
+class EntradaForm(forms.ModelForm):
+    class Meta:
+        model = Despesa
+        fields = ['categoria', 'valor', 'descricao']
+        widgets = {
+            'categoria': forms.Select(attrs={'class': 'form-control'}),
+            'valor': forms.NumberInput(attrs={'class': 'form-control', 'step': '0.01'}),
+            'descricao': forms.TextInput(attrs={'class': 'form-control', 'maxlength': 255}),
+        }
+
+
 class DespesaForm(forms.ModelForm):
     class Meta:
         model = Despesa
